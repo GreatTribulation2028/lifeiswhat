9cb3308f3394d8283c8e59ec0af2470f5f945799
GreatTribulation <ollietoto@gmail.com>
2026-03-23 15:06:55 +0800

    Update data_today.js

--- data_today.js
+++ data_today.js
@@ -1,3 +1,79 @@
 var DATA_TODAY = `
-
+23/3/2026	華漢馬爾代夫猝死 保險公司斟酌字眼拒賠敗訴	https://hk.on.cc/hk/bkn/cnt/news/20260323/bkn-20260323080015474-0323_00822_001.html
+23/3/2026	自杀式挑战！特拉奥雷进攻犯规&篮网挑战失败 提前已踩边线	https://news.zhibo8.com/nba/2026-03-23/69c08b666b118native.htm
+23/3/2026	葵涌地盤冧天秤工人墮斃 家屬殮房認屍難掩傷痛	https://hk.on.cc/hk/bkn/cnt/news/20260323/bkn-20260323091958298-0323_00822_001.html
+23/3/2026	大角咀屋苑男子單位內自縊 昏迷送院搶救	https://hk.on.cc/hk/bkn/cnt/news/20260323/bkn-20260323103510352-0323_00822_001.html
+23/3/2026	高雄越南情侶裸身倒臥床上雙亡 疑「提親遭反對」起殺機	https://www.ettoday.net/news/20260323/3136974.htm
+22/3/2026	伊朗伊斯兰革命卫队发声	https://mil.news.sina.com.cn/2026-03-22/doc-inhrvnnz9092425.shtml
+22/3/2026	高雄教會60歲婦人墜樓重傷 從6樓空中花園墜落2樓天井送醫	https://tw.news.yahoo.com/高雄教會60歲婦人墜樓重傷-從6樓空中花園墜落2樓天井送醫-071112143.html
+22/3/2026	14歲身障少女慘遭凌虐焚屍！主嫌林春雄獄中「強健體魄竟突暴斃」 名嘴感嘆：這就是因果	https://tw.news.yahoo.com/14-歲身障少女慘遭凌虐焚屍-主嫌林春雄獄中-強健體魄竟突暴斃-名嘴感嘆-050200976.html
+22/3/2026	「台灣好行」巴士司機竟毒駕！觀光署回應了	https://www.4gtv.tv/article/2026032203000002
+22/3/2026	追了10年！梅姨终于落网，申军良：遗书都写好了，黄河边坐整夜	https://m.yule.360.com/content/4972130
+22/3/2026	快訊／台中死亡車禍!聯結車右轉輾斃7 旬翁	https://www.msn.com/zh-tw/news/national/快訊-台中死亡車禍-聯結車右轉輾斃7旬翁/ar-AA1Ym1pq?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	清邁12天72隻老虎暴斃!「屍體堆成山」 燒不完園區急關閉	https://www.msn.com/zh-tw/news/world/清邁12天72隻老虎暴斃-屍體堆成山-燒不完-園區急關閉/ar-AA1WP5VO
+22/3/2026	老婦搭機暴斃！航班不折返「遺體塞廚房加熱13小時」 旅客被迫聞屍臭	https://tw.news.yahoo.com/老婦搭機暴斃-航班不折返-遺體塞廚房加熱13小時-旅客被迫聞屍臭-092400824.html
+22/3/2026	英航香港飛倫敦客機乘客猝死遺體塞廚房「加熱」13小時 傳惡臭惹乘客不滿	https://www.orangenews.hk/hongkong/VEaNeL2/英航香港飛倫敦客機乘客猝死遺體塞廚房-加熱-13小時-傳惡臭惹乘客不滿.shtml
+22/3/2026	中國電科元老孫玉病亡 時間點敏感	https://www.epochtimes.com/b5/26/3/22/n14724605.htm/amp
+22/3/2026	英航香港飛倫敦航班︱6旬婦猝死機師拒折返 逾330乘客伴屍13小時	https://www.stheadline.com/realtime-world/3555313/英航香港飛倫敦航班6旬婦猝死機師拒折返-逾330乘客伴屍13小時
+22/3/2026	彰化驚悚命案！男子疑毒品糾紛雙眼被挖空、臉削爛身亡 2主嫌遭羈押禁見	https://mnews.tw/story/amp/mm-20260322edi034
+22/3/2026	韩国财阀阴影下的娱乐圈！女星沦为玩物，230页遗书揭开血腥真相	https://m.yule.360.com/content/4972309
+22/3/2026	我的山与海：高翔做梦都没想到，孟思远的遗书竟成全了他跟方婉之	https://www.sohu.com/a/999734175_122094386
+22/3/2026	18歲男無照毒駕輾斃路人 律師答辯無殺人犯意	https://www.msn.com/zh-tw/news/national/18歲男無照毒駕輾斃路人-律師答辯無殺人犯意/ar-AA1XuIJI
+22/3/2026	韋基馬去年因喉癌離世 AI生成影像「翻生」演出新戲首張劇照曝光	https://www.hk01.com/電影/60332528/韋基馬去年因喉癌離世-ai生成影像-翻生-演出新戲首張劇照曝光
+22/3/2026	《豆腐媽媽》替身墜樓1 個月未婚妻開頻道曝未來動向	https://www.msn.com/zh-tw/entertainment/news/豆腐媽媽-替身墜樓1個月-未婚妻開頻道曝未來動向/ar-AA1WPkQS?cvid=90bf02807fc04022d5e00a8e26ca26a4&ocid=backredirect2vlp&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	當時意外造成14人死亡 #巴西 #塌橋 #電單車 #am730	https://www.facebook.com/am730hk/posts/當時意外造成14人死亡巴西-塌橋-電單車-am730/1417860400381558/
+22/3/2026	台東死亡車禍！7旬翁開車失控衝民宅受困 送醫搶救不治	https://news.ebc.net.tw/news/society/543330
+22/3/2026	曾參與記錄梅姨案 調查記者魏華猝逝 引猜測	https://www.epochtimes.com/b5/26/3/22/n14724622.htm/amp
+22/3/2026	35歲電單車網紅試車「跑山」遇禍亡 前一天工程師曾勸勿上山	https://std.stheadline.com/realtime-china/3555334/35歲電單車網紅試車跑山遇禍亡-前一天工程師曾勸勿上山
+22/3/2026	张娜拉被传“自杀”上热搜！真相竟和公司职员有关？	https://m.yule.360.com/content/4972527
+22/3/2026	北市公車輾斃老婦 大都會客運：司機非故意肇逃「視線死角導致」	https://www.msn.com/zh-tw/news/national/北市公車輾斃老婦-大都會客運-司機非故意肇逃-視線死角導致/ar-AA1XkBW0
+22/3/2026	自杀学生的父母职业分布，排在第一名的竟是……	https://www.163.com/dy/article/KOLJPSC20556BUK2.html
+22/3/2026	2NE1朴春怒了! 指控「Dara為了吸毒誣陷我」 手寫信反擊團員做偽證	https://www.msn.com/zh-tw/entertainment/news/2ne1朴春怒了-指控-dara為了吸毒誣陷我-手寫信反擊團員做偽證/ar-AA1XqcA8?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	台中停車意外釀空軍士兵死亡 空軍： 將持恆實施行車安全宣教	https://www.msn.com/zh-tw/news/national/台中停車意外釀空軍士兵死亡-空軍-將持恆實施行車安全宣教/ar-AA1XKwp8?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	用戶深陷AI伴侶虛幻愛戀 美國父控Google Gemini助長兒子自殺	https://sunmedia.tw/news/technology/1774188299-用戶深陷AI伴侶虛幻愛戀 美國父控Google Gemini助長兒子自殺
+22/3/2026	天水圍商場廁內兩小兄弟疑遭非禮警方調查暫無人被捕- 香港 - 香港文匯網	https://www.wenweipo.com/a/202603/22/AP69bfef60e4b04d7d56d9ae36.html
+22/3/2026	美国大V吐槽：美军这么干，和自杀式袭击者有啥区别	https://www.guancha.cn/internation/2026_03_22_810984.shtml
+22/3/2026	瑞媒：雪況不穩 阿爾卑斯山滑雪季死亡增至127人	https://www.worldjournal.com/wj/amp/story/121480/9396431
+22/3/2026	「通俄門」檢察官離世 特朗普：很高興	https://www.hkej.com/dailynews/international/article/4348106/-%E9%80%9A%E4%BF%84%E9%96%80-%E6%AA%A2%E5%AF%9F%E5%AE%98%E9%9B%A2%E4%B8%96-%E7%89%B9%E6%9C%97%E6%99%AE-%E5%BE%88%E9%AB%98%E8%88%88
+22/3/2026	英航女客猝亡 被置備餐區13小時散屍臭	https://www.hkej.com/dailynews/international/article/4348102/英航女客猝亡+被置備餐區13小時散屍臭
+22/3/2026	內地電單車網紅試駕新車跑山期間發生事故身亡	https://www.singtaousa.com/2026/03/22/news/china/motorcycle-influencer-test-drive-mountain-crash-fatal/
+22/3/2026	熱刺領隊伊戈爾杜鐸因家人離世缺席賽後訪問	https://www.bastillepost.com/hongkong/article/15776469-熱刺領隊伊戈爾杜鐸因家人離世缺席賽後訪問
+22/3/2026	大便太用力「當場暈倒猝死」! 醫師曝恐怖成因： 血壓瞬間崩盤	https://www.msn.com/zh-tw/news/other/大便太用力-當場暈倒猝死-醫師曝恐怖成因-血壓瞬間崩盤/ar-AA1Z9GE5?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	乘客機上猝逝傳屍臭英航機長拒折返惹不滿- 港聞	https://epaper.tkww.hk/a/202603/23/AP69c050f4e4b04773b06a450f.html
+22/3/2026	天母大葉高島屋「一個月內2起墜樓」! 婦重摔慘死 當地居民震驚	https://www.msn.com/zh-tw/news/national/天母大葉高島屋-一個月內2起墜樓-婦重摔慘死-當地居民震驚/ar-AA1XHfGe
+22/3/2026	快訊／桃園女15樓商辦墜下OHCA! 倒血泊送醫	https://www.msn.com/zh-tw/news/national/快訊-桃園女15樓商辦墜下ohca倒血泊送醫/ar-AA1XkRyh?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	乘客機上猝逝遺體加熱13小時! 空服員崩潰	https://www.msn.com/zh-tw/news/world/乘客機上猝逝遺體加熱13小時-空服員崩潰/ar-AA1Z9PDJ?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	一場車禍臥床10年！澎湖前議長藍俊逸器官衰竭亡 醫曝72小時生死線	https://health.setn.com/news/1811321
+22/3/2026	結束訓練回家途中遇死劫！18歲排球選手被火車撞斃…死因竟跟耳機有關	https://www.setn.com/m/ampnews.aspx?NewsID=1811343
+22/3/2026	「超自然檔案」女星離世 過世1個月死訊才曝光	https://www.worldjournal.com/wj/story/121232/9396511
+22/3/2026	英航婦人遺體放備餐區疑傳異味 乘客猝死如何處理原來有國際標準？｜延伸閱讀	https://news.tvb.com/tc/world/69bfbb0767ae60978fd7bf5c/國際-英航婦人遺體放備餐區疑傳異味-乘客猝死如何處理原來有國際標準｜延伸閱讀
+22/3/2026	全球市場恐懼來襲！川普48小時死亡通牒直指伊朗電力命脈 中東火藥庫恐全面引爆	https://www.ftnn.com.tw/news/532071
+22/3/2026	港大研發血液檢測預測系統！6ml血即提早15年預知六種心血管病風險！中風、猝死病發有預警	https://hk.ulifestyle.com.hk/topic/detail/20091811/全球頭號殺手有得避-港大首創ai檢測-只需抽-毫升血-提早-年預知中風-猝死-打工仔必睇
+22/3/2026	拉屎等于自杀，它屎拉到一半就意外身亡，简直太悲催了！	https://m.sohu.com/a/999857698_122418371?scm=10001.325_13-325_13.0.0-0-0-0-0.5_1334
+22/3/2026	新店BMW自撞護欄!車頭全爛撞歪站牌 毒駕男「癱坐車內補幾口」 送辦	https://www.msn.com/zh-tw/news/national/新店bmw自撞護欄-車頭全爛撞歪站牌-毒駕男-癱坐車內補幾口-送辦/ar-AA1YYf7A
+22/3/2026	快訊／83歲翁詭異墜坡亡「兒買農藥遭收押」 解剖結果出爐	https://www.msn.com/zh-tw/news/other/快訊-83歲翁詭異墜坡亡-兒買農藥遭收押-解剖結果出爐/ar-AA1YXeaO?cvid=69bc25d7d94144c089adf0e1e36366e0&ocid=wispr&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	快訊／19樓墜B5!南港建案工安意外 61歲工人軀幹斷裂慘死	https://www.msn.com/zh-tw/news/national/快訊-19樓墜b5南港建案工安意外-61歲工人軀幹斷裂慘死/ar-AA1YSbyg
+22/3/2026	大便太用力「馬桶上猝死」！醫示警3類人注意 出現4前兆快放鬆	https://health.setn.com/news/1811389
+22/3/2026	男子馬爾代夫浮潛猝死｜保險公司拒賠80萬 法院判決震撼	https://www.weekendhk.com/最新焦點/最新熱話速遞-保險理賠-旅遊意外-3391355/
+22/3/2026	香港飛倫敦｜英航客機女乘客猝逝 傳被放錯「這區」13小時 全機聞異味	https://www.gotrip.hk/網絡熱話/英國航空-航班意外-機上死亡-1867786/
+22/3/2026	三位七八旬法輪功學員在迫害中含冤離世	https://www.epochtimes.com/b5/26/3/16/n14720511.htm/amp
+22/3/2026	「最美抗癌女神」29歲離世震驚粉絲 醫示警：多數人忽略這常見症狀	https://tw.news.yahoo.com/最美抗癌女神-29歲離世震驚粉絲-醫示警-多數人忽略這常見症狀-021704156.html
+22/3/2026	悚！高雄情侶躺床雙亡「1男1女全身刀傷」	https://tw.news.yahoo.com/悚-高雄情侶躺床雙亡-1男1女全身刀傷-020758629.html
+22/3/2026	被拐孩子父亲申军良称寻子路上曾绝望写好遗书，想用自己命换回儿子	https://c.m.163.com/news/v/VCNMN2B14.html?spss=backflow-index-hotlist
+22/3/2026	大埔馬路「自殺式襲擊」 擒青男衝出攔腰撞私家車 自轉兩圈倒地 大難不死全靠一幸運元素｜Juicy叮	https://www.singtao.ca/googleamp/amp.php?region=toronto_hk&cat=139&post_id=7452798
+22/3/2026	才被警告「會暴斃」! 江宏恩突缺席記者會 錢薇娟揭他錄影真實狀態	https://www.msn.com/zh-tw/entertainment/news/才被警告-會暴斃-江宏恩突缺席記者會-錢薇娟揭他錄影真實狀態/ar-AA1Z1ZOu
+22/3/2026	郵輪台籍遊客中風昏迷 空勤救援後送	https://www.ksnews.com.tw/e/84436
+22/3/2026	大便太用力猝死！醫驚揭「氣吐完才出事」 3關鍵救命	https://health.ettoday.net/news/3136805
+22/3/2026	排便太用力...馬桶上猝死！醫示警2徵兆：大腦供血不足的最後通牒	https://tw.news.yahoo.com/排便太用力-馬桶上猝死-醫示警2徵兆-大腦供血不足的最後通牒-032308678.html
+22/3/2026	貴州赫章縣嚴重交通意外 釀2名學生死亡11人受傷	https://www.stheadline.com/realtime-china/3555534/貴州赫章縣嚴重交通意外-釀2名學生死亡11人受傷
+22/3/2026	反器官活摘隔天猝逝！火雲邪神梁小龍爆「離世2個月未安葬」遺孀發聲了	https://star.setn.com/news/1811460
+22/3/2026	拉脫維亞籍女子陷入「假軍官」 甜言蜜語淪國際販毒工具	https://www.msn.com/zh-tw/news/national/拉脫維亞籍女子陷入-假軍官-甜言蜜語-淪國際販毒工具/ar-AA1XtwlN?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+22/3/2026	英航香港飛倫敦航班有乘客離世遺體放發熱備餐區| 獨媒報導	https://www.inmediahk.net/node/國際/英航香港飛倫敦航班有乘客離世-遺體放發熱備餐區
+22/3/2026	屯門公路貨櫃車與消防車相撞後翻側 司機昏迷送院 出九龍受阻	https://www.hk01.com/突發/60333170/屯門公路貨櫃車與消防車相撞後翻側-司機昏迷送院-出九龍受阻
+22/3/2026	高雄岡山驚傳雙屍命案！越籍情侶陳屍反鎖套房 全身血跡明顯死亡	https://n.yam.com/Article/20260323734093
+22/3/2026	宏福苑聽證會｜殉職英雄最後身影曝光！何偉豪「落單」真相大白：為護送居民撤離與同袍失散，吸入過量CO不幸墮樓。	https://hk.news.yahoo.com/宏福苑聽證會-殉職英雄最後身影曝光-何偉豪-落單-真相大白-040731717.html
+22/3/2026	土耳其新一代自殺式無人艇「食人魚」測試成功 多層次指管＋海陸空聯網	https://tw.news.yahoo.com/土耳其新-代自殺式無人艇-食人魚-測試成功-多層次指管-043251533.html
+21/3/2026	英航香港飛倫敦航班乘客猝逝機長不折返遺體安置機尾13小時傳惡臭- 國際	https://www.dotdotnews.com/a/202603/22/AP69bf8747e4b0c32d4f6ca076.html
+		
+		
 `;		
