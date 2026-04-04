2a1fc60d15bfab80050def83d3f09876ad3aaf19
GreatTribulation <ollietoto@gmail.com>
2026-04-04 13:36:02 +0800

    Update data_today.js

--- data_today.js
+++ data_today.js
@@ -1,5 +1,101 @@
 var DATA_TODAY = `
-
-
+4/4/2026	(有片) 珍惜生命│情海翻波留遺書尋死觀塘女子企跳獲救- 香港 - 香港文匯網	https://www.wenweipo.com/a/202604/03/AP69cf5011e4b0b49ad1b529cd.html
+4/4/2026	「護理女神」謝侑芯猝逝163天！黃明志突發新歌揭遺憾 寫給離去的人聽引聯想	https://n.yam.com/Article/20260403115077
+4/4/2026	澳洲男星康拉德比恩史蒂芬（Konrad Bien Stephen）於當地時間3月30日在墨爾本猝逝，年僅35歲，其經紀公司2日正式證實噩耗，不過死因未對外公開。	https://tw.news.yahoo.com/參加戀愛實境秀走紅-35歲澳洲男星猝逝-7天前最後發文曝光-070522982.html
+4/4/2026	網紅「雨鑫媽媽」抗癌4年病逝！享年40歲留罕病女10萬粉絲心碎| 生活	https://www.nownews.com/news/6803391
+4/4/2026	台中新光三越專櫃店長猝死！同事爆料：人力不足連上好幾天	https://tw.news.yahoo.com/台中新光三越專櫃店長猝死-同事爆料-人力不足連上好幾天-071042876.html
+4/4/2026	鄧麗君逝世前情況絕密曝光 泰管家披露內幕爆被要求改名 - 東張+	https://www.mytvsuper.com/tc/scoopplus/entertainment/e-news/17025831640845/娛樂-鄧麗君逝世前情況絕密曝光-泰管家披露內幕爆被要求改名
+4/4/2026	有片| 珍惜生命| 觀塘廣場妙齡女子危坐12樓欄杆消防封路開氣墊多條巴士線改道- 東張+	https://www.mytvsuper.com/tc/scoopplus/news/headlines/17031014394505/重點新聞-有片---珍惜生命---觀塘廣場妙齡女子危坐12樓欄杆--消防封路開氣墊多條巴士線改道--
+4/4/2026	中和抽水站工安意外！65歲工人高處墜落 同事發現已死亡	https://news.tvbs.com.tw/local/3169137
+4/4/2026	全球實境秀粉絲陷入哀傷！曾因參加澳洲版《單身女郎》（The Bachelorette Australia）而走紅的真人秀明星康拉德邊史蒂芬（Konrad Bien-Stephen），驚傳於墨爾本不幸辭世，...	https://news.tvbs.com.tw/entertainment/3169110
+4/4/2026	心寒！陸女低血糖暈倒 尪零反應「冷眼旁觀3分鐘」關電腦抱女兒走了	https://tw.news.yahoo.com/心寒-陸女低血糖暈倒-尪零反應-冷眼旁觀3分鐘-關電腦抱女兒走了-074531040.html
+4/4/2026	謝侑芯猝死半年 黃明志再發聲曝「思念與遺憾」	https://tw.news.yahoo.com/謝侑芯猝死半年-黃明志再發聲曝-思念與遺憾-074836872.html
+4/4/2026	拳擊(影)／悲劇畫面！「TKO後送醫不治」拳手身亡 「為何不早停？」裁判引眾怒	https://tw.sports.yahoo.com/news/拳擊-影-悲劇畫面-tko後送醫不治-拳手身亡-082501137.html
+4/4/2026	每到晚上就腳痛 一度誤診足底筋膜炎⋯真相竟是罕見疾病「瀨川氏症」	https://www.worldjournal.com/wj/amp/story/121238/9421142
+4/4/2026	港星許紹雄「癌症引多重器官衰竭」病逝！享壽76歲 家人證實： 安詳離世	https://www.msn.com/zh-tw/entertainment/news/港星許紹雄-癌症引多重器官衰竭-病逝-享壽76歲-家人證實-安詳離世/ar-AA1Pj93v?cvid=5b9f91fd5bfc4ac1a6361184b2c478d2&ocid=ARWLCHR
+4/4/2026	外送員鬆電門「舉右手」…倒下被輾成2截 專家曝致命關鍵： 切斷動力	https://www.msn.com/zh-tw/news/national/外送員鬆電門-舉右手-倒下被輾成2截-專家曝致命關鍵-切斷動力/ar-AA1ZRN9a
+4/4/2026	高雄墜樓意外 獨居69 歲婦高處摔落身亡家屬悲認屍	https://www.msn.com/zh-tw/news/national/高雄墜樓意外-獨居69歲婦高處摔落身亡-家屬悲認屍/ar-AA1ZSODb
+4/4/2026	新北中和驚傳工安意外！工人疑墜落明顯死亡	https://news.ebc.net.tw/news/society/545169
+4/4/2026	詐騙案爆炸式增長! 北檢打詐檢察官過勞昏倒 臉色慘白送上救護車	https://www.msn.com/zh-tw/news/other/詐騙案爆炸式增長-北檢打詐檢察官過勞昏倒-臉色慘白送上救護車/ar-AA202Qu6?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	「網傳蘿拉病逝？蔡阿嘎急發聲明曬一圖破謠言，網看細節嚇呆	https://tyenews.com/2026/04/1329153/
+4/4/2026	出昏招求援中俄，以媒曝出伊朗停火谈判内幕，打残美军是正途	https://m.sohu.com/a/1005042107_121286278?scm=10001.325_13-325_13.0.0-0-0-0-0.5_1334
+4/4/2026	深圳宝安一球员突发心源性猝死，现场心血管科医生将其救回	https://m.dongqiudi.com/article/5753999.html
+4/4/2026	灣仔17歲少女暈倒家中昏迷送院不治- 港聞	https://www.dotdotnews.com/s/202604/02/AP69ce1b56e4b09ea233113524.html
+4/4/2026	杉林溪遊園車墜谷1死7傷! 揭密「三不管」 亂象觀光署將納管	https://www.msn.com/zh-tw/news/living/杉林溪遊園車墜谷1死7傷-揭密-三不管-亂象-觀光署將納管/ar-AA1ZlCEm?cvid=ea3d392de89b46d1fd2d75319797d495
+4/4/2026	影／外送員遭輾斃慘死！突倒下「比中指or求救」引熱議 提供監視器的店家回應了	https://sunmedia.tw/news/collaborative/2acUgQRl2gSuldPntgr9btUK4mmfKahkpZSFUMtNIIUC4U3WdIUjKIoe8AxhAA8QMUYwXysT
+4/4/2026	曾遭醫警告長期失眠恐暴斃！江宏恩錄影「扛沙袋」秀超狂體能	https://tw.news.yahoo.com/曾遭醫警告長期失眠恐暴斃-江宏恩錄影-扛沙袋-秀超狂體能-101047024.html
+4/4/2026	如廁心臟病發猝死! 50歲美食主持人邱晉為身分曝 客家台35 字證實了	https://www.msn.com/zh-tw/entertainment/news/美食主持人如廁猝死-客家台35字悲痛證實了/ar-AA1ZqpaL
+4/4/2026	公司3人都有動脈瘤！老闆猝逝、2男健檢驚見同部位	https://news.tvbs.com.tw/life/3169255?from=pulldownmenu_content_生活_公司3人都有動脈瘤！老闆猝逝、2男健檢驚見同部位
+4/4/2026	清明祭扫人流中，老人突然昏倒，15分钟接力救人！	https://m.thepaper.cn/newsDetail_forward_32896245
+4/4/2026	中職／知道差二壘打完全打擊 陳文杰最後超想衝! 但不敢「自殺」 拚紀錄	https://www.msn.com/zh-tw/sports/other/中職-知道差二壘打完全打擊-陳文杰最後超想衝-但不敢-自殺-拚紀錄/ar-AA1ZUVYm?cvid=69cd5c8d5a3b461e930570f60eb5f53f&ocid=hpmsn&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	珍惜生命｜淘大花園男子墮樓不治疑受金錢問題困擾- 港聞	https://www.dotdotnews.com/a/202604/03/AP69cfadb3e4b09ea233115663.html
+4/4/2026	公司3人藏腦炸彈！老闆「嘔吐後猝逝」 醫驚：2同事瘤還長同位置	https://health.ettoday.net/news/3142956
+4/4/2026	抗癌網紅「雨鑫媽媽」離世 照顧罕病女十二載 生前發文求活下去	https://www.hk01.com/即時娛樂/60337120/抗癌網紅-雨鑫媽媽-離世-照顧罕病女十二載-生前發文求活下去
+4/4/2026	「蘿拉心臟病逝」蔡阿嘎現身哀悼RIP 網友留言全都歪樓	https://today.line.me/tw/v3/posts/list/article/XYOnpml
+4/4/2026	討論牆 | 鄧麗君病逝原因曝！ 泰管家揭：運動洗澡「哮喘引發呼吸衰竭」	https://today.line.me/tw/v3/posts/list/article/j7v2eRa
+4/4/2026	李家鼎揭與李泳豪被禁探望施明離世兩日後才接獲消息- 本地 - 明周娛樂	https://www.mpweekly.com/entertainment/article/李家鼎揭與李泳豪被禁探望-施明離世兩日後才接獲
+4/4/2026	元朗錦繡花園半歲女嬰家中暈倒送院亡 38歲印傭被捕	https://std.stheadline.com/breaking-news/3559180/元朗錦繡花園半歲女嬰家中暈倒送院亡-38歲印傭被捕
+4/4/2026	25歲男住戶淘大花園墮樓亡｜因一件事尋短 現場檢獲遺書	https://www.weekendhk.com/最新焦點/最新熱話速遞-社會事件-精神健康-3400135/
+4/4/2026	「蘿拉」病逝蔡阿嘎哀悼！網歪樓：錢還沒還不能 R.I.P	https://innews.com.tw/「蘿拉」病逝蔡阿嘎哀悼！網歪樓：錢還沒還不能-r/
+4/4/2026	聖達菲令人震驚：一對夫婦被發現死亡，一名殺女者和自殺者正在接受調查	https://www.arch-web.com.tw/综合新闻/聖達菲令人震驚：一對夫婦被發現死亡，一名殺女/601587/
+4/4/2026	東張西望｜好心借10萬換死亡恐嚇 廣西債仔兄弟聯手 公安都冇符!	https://www.weekendhk.com/entertainment/東張西望-借貸-同鄉-3400132/
+4/4/2026	"华大CEO尹烨：不能把跑步和猝死强关联。这个真相，90%的人都搞错了。真正害你的是""透支+突然猛练”的蠢操作"	https://m.sohu.com/a/1002347737_114986/?pvid=000115_3w_a
+4/4/2026	元朗錦綉花園6個月大女嬰昏迷送院亡 警拘38歲印傭交重案組調查	https://www.hk01.com/突發/60337188/元朗錦綉花園6個月大女嬰昏迷送院亡-警拘38歲印傭交重案組調查
+4/4/2026	醫起看／感冒運動逼汗是玩命？醫示警：恐引爆病毒性心肌炎	https://news.ebc.net.tw/news/health/544130
+4/4/2026	大衣哥朱之文‘死亡谣言’幕后黑手获刑！57岁农民捐出8.5万赔偿金：可以骂我，别动我家人	https://m.yule.360.com/content/4998059
+4/4/2026	澳洲真人騷男星Konrad Bien-Stephen驚爆離世 7日前仍發文展笑容	https://www.hk01.com/即時娛樂/60337087/澳洲真人騷男星konrad-bien-stephen驚爆離世-7日前仍發文展笑容
+4/4/2026	網路找「通排水管」竟釀一氧化碳中毒！水電工2人+ 屋主送醫	https://www.msn.com/zh-tw/health/other/網路找-通排水管-竟釀一氧化碳中毒-水電工2人-屋主送醫/ar-AA1Mtt5y?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	33歲車房老闆因一件事不開心｜紅磡車房內懷疑自殺身亡	https://www.weekendhk.com/最新焦點/最新時事速遞-紅磡車房-懷疑自殺-3400161/
+4/4/2026	6個月女嬰家中暴斃｜印尼女傭涉虐待被捕 死因待查	https://www.weekendhk.com/最新焦點/最新熱話速遞-元朗新聞-兒童安全-3400158/
+4/4/2026	独身女子去世留下600万法院宣判 民政局成遗产管理人	https://news.china.com/socialgd/10000169/20260404/49385627.html
+4/4/2026	錦繡花園6個月大女嬰昏迷不治警拘印傭涉疏忽照顧- 香港 - 香港文匯網	https://www.wenweipo.com/a/202604/03/AP69cfd217e4b0b49ad1b53ac1.html
+4/4/2026	資深港星肺炎病逝享壽74 歲曾擔任席維斯史特龍女保鑣	https://www.msn.com/zh-tw/entertainment/news/資深港星肺炎病逝享壽74歲-曾擔任席維斯史特龍女保鑣/ar-AA1ZMe9E?cvid=69cfa35aeb804ffabb51fb155bd7da92&ocid=wispr&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	馬鞍山富寶花園有人墮樓 當場不治	https://hk.on.cc/hk/bkn/cnt/news/20260404/bkn-20260404013827201-0404_00822_001.html
+4/4/2026	施明離世｜大仔李泳漢驚爆鼎爺遭李泳豪夫婦監控 內情曝光揭300萬被迫分身家？	https://hk.ulifestyle.com.hk/topic/detail/20093553/施明離世-大仔李泳漢驚爆鼎爺遭李泳豪夫婦監控-內情曝光揭-萬被迫分身家
+4/4/2026	小區升降機急墜18樓 致2名檢修工人死亡	https://hk.on.cc/hk/bkn/cnt/news/20260404/bkn-20260404040554410-0404_00822_001.html
+4/4/2026	阿富汗發生5.9級地震 造成8人死亡	https://www.epochtimes.com/b5/26/4/3/n14734217.htm/amp
+4/4/2026	伊朗戰爭｜阿聯酋攔截飛行物 碎片墜落引設施起火至少1死多傷	https://www.hk01.com/即時國際/60337204/伊朗戰爭-阿聯酋攔截飛行物-碎片墜落引設施起火至少1死多傷
+4/4/2026	死亡率達13.7% 不是老人專利 30歲男皮蛇纏身面癱	https://tw.news.yahoo.com/死亡率達13-7-不是老人專利-30歲男皮蛇纏身面癱-220026897.html
+4/4/2026	HeartSciences報告營運長Mark Hilz逝世,暫不尋求即時接替人選 作者 Investing.com	https://hk.investing.com/news/sec-filings/article-93CH-1394816
+4/4/2026	原廉署首席調查主任Michael Burley逝世	https://www.hkej.com/instantnews/current/article/4365253/原廉署首席調查主任Michael+Burley逝世
+4/4/2026	國民黨老將發病10分鐘猝逝！死前異常舉動曝光 醫揭致命原因「難救」	https://tw.news.yahoo.com/國民黨老將發病10分鐘猝逝-死前異常舉動曝光-醫揭致命原因-難救-000000325.html
+4/4/2026	頭牙「龍抬頭」 福德祠擠爆嬤買麻糬喊快昏倒	https://www.msn.com/zh-tw/news/living/頭牙-龍抬頭-福德祠擠爆-嬤買麻糬喊快昏倒/ar-AA1Z2zXw?cvid=69c165aee4c3448cbc9511cb727a7ac7&ocid=mobilepwa&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	苗栗縣府大樓凌晨男性科長倒臥身亡	https://www.msn.com/zh-tw/news/national/苗栗縣府大樓凌晨男性科長倒臥身亡/ar-AA1HqPpo?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	員工猝死後 百貨薪資3.2萬、久站10小時	https://www.msn.com/zh-tw/news/national/員工猝死後-百貨薪資3-2萬-久站10小時/ar-AA204aKL?cvid=69cfe415b1f74736996661cf6c4e51a2&ocid=hpmsn&apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	《冬去春来》大结局！原来陶亮亮的遗书，揭开了沈冉冉的另一面	https://m.sohu.com/a/1005273111_121948378?scm=10001.325_13-325_13.0.0-0-0-0-0.5_1334
+4/4/2026	阿富汗東北部5.8級地震 至少8死1傷	https://news.tvb.com/tc/world/69d05d0bbec86a88cbeb9813/國際-阿富汗東北部5.8級地震-至少8死1傷
+4/4/2026	柯文哲法庭醜態變網路迷因素材、淪笑柄 律師： 作法自斃	https://www.msn.com/zh-tw/news/national/柯文哲法庭醜態變網路迷因素材-淪笑柄-律師-作法自斃/ar-AA1ZRTaz?apiversion=v2&domshim=1&noservercache=1&noservertelemetry=1&batchservertelemetry=1&renderwebcomponents=1&wcseo=1
+4/4/2026	情牽蘭陽一甲子 秘克琳神父離世	https://tw.news.yahoo.com/情牽蘭陽-甲子-秘克琳神父離世-021204278.html
+4/4/2026	通州街公園球場男子暈倒 昏迷送院救治	https://hk.on.cc/hk/bkn/cnt/news/20260404/bkn-20260404091918802-0404_00822_001.html
+4/4/2026	（有片）珍惜生命｜女子危坐觀塘廣場欄杆對峙逾2小時安全返回地面- 港聞	https://www.dotdotnews.com/s/202604/03/AP69cf4956e4b09ea233114b26.html
+4/4/2026	往生互助會變《死亡賭局》直擊台中「咒死街」現況	https://www.msn.com/zh-tw/news/other/往生互助會變-死亡賭局-直擊台中-咒死街-現況/ar-AA2077uM
+4/4/2026	揪心到窒息！老人院内猝死终获救 全场沸腾：真救活了！	https://i.ifeng.com/c/8s2kiZuF8Q1
+4/4/2026	烏稱俄空襲釀8死 澤連斯基籲復活節停火	https://www.tdm.com.mo/zh-hant/news-detail/1191225?category=all&isvideo=false
+4/4/2026	同公司3人都中動脈瘤！2男「瘤長同部位」前老闆嘔吐猝逝…醫喊「1症狀」是警訊	https://www.4gtv.tv/article/2026040410000005
+4/4/2026	河北杀妻案︱被揭发偷食后殴毙老婆，狠夫金昊已执行死刑	http://www.stnn.cc/c/2026-04-04/4052889.shtml
+4/4/2026	施明離世丨身後事風波升級 李泳漢爆弟弟曾索300萬 斥李泳豪夫婦監控李家鼎	https://std.stheadline.com/film-drama/3559257/施明離世丨身後事風波升級-李泳漢爆弟弟曾索300萬-斥李泳豪夫婦監控李家鼎
+4/4/2026	河北殺妻案︱拳腳齊施整夜監生毆斃老婆 狠夫金昊已執行死刑	https://std.stheadline.com/realtime-china/3559264/河北殺妻案拳腳齊施整夜監生毆斃老婆-狠夫金昊已執行死刑
+4/4/2026	【酒驾吸毒撞毙骑士】“不能沉浸在悲痛中” 遗孀：我会振作或将尝试直播带货	https://news.seehua.com/post/1463458
+4/4/2026	中環男途人捱的士撞 一度昏迷送院救治	https://www.stheadline.com/breaking-news/3559270/中環男途人捱的士撞-一度昏迷送院救治
+4/4/2026	「超偶」女星媽媽抗癌6年！不敵病魔急遽惡化失智病逝	https://stars.udn.com/star/story/10089/9422107?from=udn-catenewnews_ch1022
+4/4/2026	施明離世丨李泳漢通話紀錄揭封鎖李泳豪真相演變羅生門 爆弟婦主動結識「整散頭家」內幕	https://www.stheadline.com/film-drama/3559265/施明離世丨李泳漢通話紀錄揭封鎖李泳豪真相演變羅生門-爆弟婦主動結識整散頭家內幕
+4/4/2026	32歲博主嘴唇發紫粉絲催檢揭心臟2次停搏 醫：跳停不重啟即猝死	https://www.hk01.com/大國小事/60337108/32歲博主嘴唇發紫粉絲催檢揭心臟2次停搏-醫-跳停不重啟即猝死
+4/4/2026	伊朗疑利用華企獲得零件 重建自殺式無人機隊	https://hk.on.cc/hk/bkn/cnt/news/20260404/bkn-20260404120046526-0404_00822_001.html
+3/4/2026	打詐前線亮紅燈！北檢檢察官疑過勞昏倒 案件爆量釀人力危機	https://tw.news.yahoo.com/打詐前線亮紅燈-北檢檢察官疑過勞昏倒-案件爆量釀人力危機-021700424.html
+3/4/2026	马鞍山富宝花园七旬翁堕楼 倒卧大厦对开当场气绝	https://www.singtao.ca/7465369/2026-04-03/news-馬鞍山富寶花園七旬翁墮樓　倒臥大廈對開當場氣絕/?variant=zh-cn&appedition=vancouver
+3/4/2026	导演申圣勋被发现昏迷家中猝逝!享年40岁 友人恸:现场没留遗书	https://ent.sina.cn/2026-04-03/detail-inhteyic3806169.d.html?oid=谷歌霸屏优化【电报e10838】google代发留痕[K4mF7xZ2t9].vnc&vt=4
+3/4/2026	新蒲崗男子單位內暈倒 朋友發現已來遲	https://hk.on.cc/hk/bkn/cnt/news/20260403/bkn-20260403130154962-0403_00822_001.html
+3/4/2026	觀塘廣場女子企圖跳樓 談判專家勸服 (15:45) - 20260403 - 港聞	https://news.mingpao.com/ins/港聞/article/20260403/s00001/1775197192498/觀塘廣場女子企圖跳樓-談判專家勸服
+3/4/2026	交通事故死亡人數一季度接近歷史新低	https://www.singtaousa.com/2026/04/03/news/usa/traffic-accident-deaths-historic-low/
+3/4/2026	中山美穗離世逾一年 獨生子放棄繼承遺產	https://hk.on.cc/hk/bkn/cnt/entertainment/20260403/bkn-20260403182600263-0403_00862_001.html
+3/4/2026	25歲仔陷財困 淘大花園墮樓亡	https://hk.on.cc/hk/bkn/cnt/news/20260403/bkn-20260403193604127-0403_00822_001.html
+3/4/2026	紅磡車房負責人生活不快 疑吸電單車廢氣身亡	https://hk.on.cc/hk/bkn/cnt/news/20260403/bkn-20260403214043521-0403_00822_001.html
+3/4/2026	早睡早起反增死亡風險！研究揭黃金入睡時段 睡足8小時也易患心血管病？	https://ccue.singtao.ca/2026-04-03/早睡早起反增死亡風險！研究揭黃金入睡時段-睡足8/1093341
+3/4/2026	紅磡鶴園街33歲老闆倒斃店舖 死因待查	https://www.singtao.ca/7465225/2026-04-03/news-紅磡鶴園街33歲老闆倒斃店舖+死因待查/
+3/4/2026	美情報公司：伊朗藉中港企獲取無人機零件	https://www.ntdtv.com/b5/2026/04/03/a104082818.html/amp
+3/4/2026	錦繡花園昏迷女嬰送院亡 警拘38歲外傭 案件交重案組調查 (22:43) - 20260403 - 港聞	https://news.mingpao.com/ins/港聞/article/20260403/s00001/1775228015394/錦繡花園昏迷女嬰送院亡-警拘38歲外傭-案件交重案組調查
+2/4/2026	珍惜生命｜觀塘女子企跳 消防氣墊戒備	https://www.singtao.ca/7464831/2026-04-02/news-珍惜生命｜觀塘女子企跳++消防氣墊戒備/
+2/4/2026	活士被捕 | 車禍後聯絡特朗普？活士上銬前稱與總統通話 密錄器畫面曝光	https://www.singtao.ca/7464848/2026-04-02/news-活士被捕+|+車禍後聯絡特朗普？活士上銬前稱與總統通話+密錄器畫面曝光/
+		
 
 `;		
